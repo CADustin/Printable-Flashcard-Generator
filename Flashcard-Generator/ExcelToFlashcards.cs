8ed594b35ba53cc1ec624e0bb157510f22bae6be
Dustin Ellis <9056725+CADustin@users.noreply.github.com>
2024-05-04 19:01:53 -0700

    More project setup

--- Flashcard-Generator/ExcelToFlashcards.cs
+++ Flashcard-Generator/ExcelToFlashcards.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Flashcard_Generator
+﻿namespace Flashcard_Generator
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
     internal class ExcelToFlashcards
     {
         string QuestionColumn { get; set; }

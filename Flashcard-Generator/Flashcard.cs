d06577a4239a7891e221762dd9e997b5c8feb568
Dustin Ellis <9056725+CADustin@users.noreply.github.com>
2024-05-04 19:20:11 -0700

    Added a class representing a Flashcard

---
+++ Flashcard-Generator/Flashcard.cs
@@ -0,0 +1,40 @@
+﻿// <copyright file="Flashcard.cs" company="Dustin Ellis">
+// Copyright (c) Dustin Ellis. All rights reserved.
+// </copyright>
+
+namespace Flashcard_Generator
+{
+    /// <summary>
+    /// Flashcard object.
+    /// </summary>
+    internal struct Flashcard
+    {
+        /// <summary>
+        /// Gets or sets the Question.
+        /// </summary>
+        public string Question{ get; set; }
+
+        /// <summary>
+        /// Gets or sets the Answer.
+        /// </summary>
+        public string Answer { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Chapter number that the material originated from.
+        /// </summary>
+        public int Chapter { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Section number within the <see cref="Chapter"/> the material originated from.
+        /// </summary>
+        public int Section { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Priority of the material.
+        /// </summary>
+        /// <remarks>
+        /// The higher the number the greater the importance of the material.
+        /// </remarks>
+        public int Priority { get; set; }
+    }
+}
